b27a86f4b0fa276ccd343a21a5a8dc85a2717d30
Sanchez <esancheza@edu.upolitecnica.cr>
2021-08-03 19:11:28 -0600

    Inicia proyecto

---
+++ src/poofiguras/principal.java
@@ -0,0 +1,12 @@
+
+package poofiguras;
+
+public class principal {
+    public static void main(String[] args){
+        
+    figuras datos= new figuras();
+    datos.cuadrado();
+    datos.triangulo();
+    
+    } 
+}
